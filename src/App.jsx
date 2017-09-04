c30986e00da2693a46a1ec8246cdea400268c67b
Paul Carrillo <paulcarrillo05@gmail.com>
2017-09-03 21:31:46 -0600

    gallery and profile component completed. app was styled. There is issues with returning the json from the api

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,45 @@
   import React, { Component } from 'react';
   import './App.css';
   import { FormGroup ,FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
+  import Profile from './Profile';
+  import Gallery from './Gallery';
 
   class App extends Component {
     constructor(props) {
       super(props);
         this.state = {
-          query: ''
+          query: '',
+          artist: null,
+          tracks: ''
         }
     }
 
-    search() {
+  search() {
       console.log('this.state', this.state);
       const BASE_URL = 'https://api.spotify.com/v1/search?';
-      const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
-      console.log('FETCH_URL', FETCH_URL);
+      let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+      const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
+
+      fetch(FETCH_URL, {
+        method: 'GET'
+      })
+      .then(response => response.json())
+      .then(json => {
+        const artist = json.artists.items[0];
+        console.log('artist', artist);
+        this.setState({artist});
+
+        FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=US&`
+        fetch(FETCH_URL, {
+          method: 'GET'
+        })
+        .then(response => response.json())
+        .then(json => {
+          console.log('artist\'s top top-tracks:', json);
+          const { tracks } = json;
+          this.setState({tracks});
+        })
+      });
     }
 
     render() {
@@ -39,18 +64,23 @@
               </InputGroup.Addon>
             </InputGroup>
           </FormGroup>
-          <div>
-            <div className="Profile">
-              <div>Artist Picture</div>
-              <div>Artist Name</div>
+          {
+            this.state.artist !== null
+            ?
+            <div>
+              <Profile
+                artist={this.state.artist}
+              />
+              <Gallery
+                tracks={this.state.tracks}
+              />
             </div>
+            : <div></div>
+          }
+
           </div>
-        <div className="Gallery">
-          Gallery
-        </div>
-        </div>
-      )
+        )
+      }
     }
-  }
 
   export default App;
